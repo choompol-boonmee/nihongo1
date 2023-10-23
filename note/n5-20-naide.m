a89e62692931d9d3379ebf3897fcedea6d7a8811
Choompol Boonmee <dr.choompol.boonmee@gmail.com>
2023-10-23 11:27:59 +0700

    Create n5-20-naide.m

---
+++ note/n5-20-naide.m
@@ -0,0 +1,87 @@
+## 文型：AないでB
+<https://nihongokyoshi-net.com/2018/04/20/jlptn5-grammar-naide/>
+
+## [意味]
+
+Aをしない状態でBをする。
+
+## [英訳]
+To do B without doing A
+
+## [接続]
+V(ない形) + ないで
+
+のむ →　のまないで
+
+たべる → たべないで
+
+みる → みないで
+
+## [JLPT レベル]
+N5
+
+## 例文
+
+今朝、朝ごはんを食べないで学校に来ました。
+
+This morning, I came to school without eating breakfast.
+
+私は毎日、電気を消さないで寝ます。
+
+I sleep without turning off the lights every day.
+
+最近、涼しいので、エアコンをつけないで寝ます。
+
+I sleep without turning on the air conditioner because it is cooler these days.
+
+傘を持たないで出かけます。
+
+I will go out without an umbrella.
+
+コーヒーに砂糖を入れないで飲みます。
+
+I drink coffee without sugar.
+
+宿題をしないで学校に行ったら怒られました。
+
+I went to school without doing my homework, and my teacher got angry with me.
+
+昨日、お風呂に入らないで寝てしまった。
+
+I fell asleep without taking a bath last night.
+
+友達にもらった本を読まないで売ってしまった。
+
+I sold the book that my friend gave me without reading it.
+
+明日は試験なので、今晩は寝ないで勉強します。
+
+I have an exam tomorrow, so I'm going to study without sleeping tonight.
+
+熱があったので、お風呂に入らないで寝ました。
+
+I had a fever, so I went to bed without taking a bath.
+
+体調が悪いときは、無理しないで休んでください。
+
+If you are not feeling well, please rest without straining yourself.
+
+わからないことがあったら、遠慮しないで聞いてください。
+
+If you have any questions, please don't hesitate to ask.
+
+財布を持たないで来てしまった。
+
+I came here without my wallet.
+
+辞書を使わないで、一人でやりました。
+
+I did not use a dictionary and did the work by myself.
+
+朝寝坊して急いでいたので、歯磨きをしないで学校に来ちゃった。
+
+I overslept and was in a hurry, so I came to school without brushing my teeth.
+
+今日は学校に行かないで、どこに行っていたの？
+
+Where did you go without going to school today?
